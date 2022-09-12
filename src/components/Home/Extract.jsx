fdf6edaddf2d6b5bc376e5c6e867bd0a2ed8e3f2
mateus <mateus.tigreewallet@gmail.com>
2022-09-12 15:30:45 -0300

    projeto finalizado

--- src/components/Home/Extract.jsx
+++ src/components/Home/Extract.jsx
@@ -6,71 +6,87 @@ export default function Extract(props) {
   const { balance } = useContext(Context);
 
   return (
-    <Main>
+    <Container>
       <ul>
         {props.extract.map((element, indice) => {
           return (
             <li key={indice}>
               <div>
                 <p>{element.date}</p>
+                <h2>{element.description}</h2>
               </div>
-              <h2>{element.description}</h2>
               <p className={element.type}>{element.value}</p>
             </li>
           );
         })}
       </ul>
       <Footer>
-        <h3>SALDO</h3>
-        <h4>{Number(balance).toFixed(2)}</h4>
+        <h2>SALDO</h2>
+        <h3>{Number(balance).toFixed(2)}</h3>
       </Footer>
-    </Main>
+    </Container>
   );
 }
 
-const Main = styled.div`
+const Container = styled.div`
+  width: 32.6rem;
+  height: 44.6rem;
+  background-color: white;
   display: flex;
   flex-direction: column;
-
-  li {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 0.5rem;
+  margin: 2.2rem auto;
+  position: relative;
   ul {
-    margin: 0 auto;
-    width: 90%;
-
-    color: black;
-  }
-
-  div {
     display: flex;
-    gap: 1rem;
-  }
-
-  p {
-    font-weight: 400;
+    height: 95%;
+    flex-direction: column;
+    margin-bottom: 5rem;
+    overflow-y: scroll;
+    width: 95%;
+    margin: 0 auto;
+    padding-top: 2.3rem;
     font-size: 1.6rem;
-
-    color: black;
-  }
-
-  .exit {
-    color: red;
-  }
-  .entry {
-    color: green;
-  }
-
-  h2 {
-    font-weight: 400;
-    font-size: 1.6rem !important;
-    line-height: 1.9rem;
-
-    color: #000000 !important;
+    div {
+      display: flex;
+      gap: 0.5rem;
+    }
+    li {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 2.3rem;
+      p {
+        color: #c6c6c6;
+      }
+      .entry {
+        color: #03ac00;
+      }
+      .exit {
+        color: #c70000;
+      }
+    }
   }
 `;
 
-const Footer = styled.footer``;
+const Footer = styled.footer`
+  bottom: 0;
+  left: 1rem;
+  display: flex;
+  width: 95%;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+
+  h2 {
+    font-size: 1.7rem;
+    font-weight: bold;
+    font-family: "Raleway", sans-serif;
+  }
+
+  h3 {
+    font-size: 1.7rem;
+    color: #03ac00;
+  }
+`;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,10 +7,12 @@ import porta from "../../imgs/porta.svg";
 import saida from "../../imgs/saida.svg";
 import entrada from "../../imgs/Entrada.svg";
 import Extract from "./Extract.jsx";
+import NoExtract from "./NoExtract";
 
 export default function Home() {
   const navigate = useNavigate();
-  const { name, token, email } = useContext(Context);
+  const { name, token, email, setName, setToken, setEmail } =
+    useContext(Context);
   const [extract, setExtract] = useState([]);
 
   const config = {
@@ -31,126 +33,79 @@ export default function Home() {
   }, []);
 
   return (
-    <Container>
-      <div>
+    <>
+      <Header>
         <h2>Olá, {name}</h2>
-        <img onClick={() => navigate("/")} src={porta} />
-      </div>
-      <div className="registry">
-        {extract.length > 0 ? (
-          <Extract extract={extract} />
-        ) : (
-          <p className="p">Não há registros de entrada ou saída</p>
-        )}
-      </div>
-      <Footer>
-        <div>
-          <img onClick={() => navigate("/newEntry")} src={entrada} />
-          <p>
-            Nova <br /> entrada
-          </p>
-        </div>
+        <img
+          onClick={() => {
+            setToken("");
+            setName("");
+            setEmail("");
+            navigate("/");
+          }}
+          src={porta}
+        />
+      </Header>
 
-        <div>
+      {extract.length > 0 ? (
+        <Extract extract={extract} />
+      ) : (
+        <NoExtract></NoExtract>
+      )}
+
+      <Footer>
+        <button>
+          <img onClick={() => navigate("/newEntry")} src={entrada} />
+          <h2>
+            Nova <br /> entrada
+          </h2>
+        </button>
+
+        <button>
           <img onClick={() => navigate("/newExit")} src={saida} />
-          <p>
+          <h2>
             Nova <br /> saída
-          </p>
-        </div>
+          </h2>
+        </button>
       </Footer>
-    </Container>
+    </>
   );
 }
 
-const Container = styled.main`
-  height: 100%;
-  width: 100%;
-
+const Header = styled.header`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-
-  div {
-    width: 90%;
-
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-top: 2.5rem;
-  }
-
+  justify-content: space-between;
+  width: 32.6rem;
+  margin: 2.5rem auto 0 auto;
   h2 {
-    font-weight: 700;
+    color: white;
     font-size: 2.6rem;
-
-    color: #ffffff;
+    font-weight: bold;
+    font-family: "Raleway", sans-serif;
   }
-
-  .registry {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    width: 90%;
-    min-height: 70vh;
-
-    background-color: #ffffff;
-    border-radius: 5px;
-  }
-
-  .p {
-    font-weight: 400;
-    font-size: 2rem;
-    text-align: center;
-
-    padding: 0px 20%;
-
-    color: #868686;
-  }
-
-  .saldo {
-    font-weight: 700;
-    font-size: 1.7rem;
-
-    color: #000000;
+  img {
+    cursor: pointer;
   }
 `;
 
-const Footer = styled.footer`
-  height: 90%;
-  width: 98%;
-
+const Footer = styled.main`
+  width: 32.6rem;
+  margin: 0 auto;
   display: flex;
-  justify-content: space-around;
-  align-items: center;
-
-  margin-top: -1.5rem;
-  margin-bottom: 1rem;
-
-  div {
+  justify-content: space-between;
+  button {
     width: 15.5rem;
     height: 11.4rem;
-
-    background: #a328d6;
-    border-radius: 5px;
-
-    display: flex;
-    align-items: flex-start;
-    justify-content: space-around;
-
-    flex-direction: column;
-  }
-
-  img {
-    margin-left: 1rem;
-  }
-
-  p {
-    margin-left: 1rem;
-    font-weight: 700;
+    background-color: #a328d6;
+    border: none;
+    border-radius: 0.5rem;
     font-size: 1.7rem;
-
-    color: #ffffff;
+    font-weight: bold;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    text-align: left;
+    cursor: pointer;
   }
 `;
